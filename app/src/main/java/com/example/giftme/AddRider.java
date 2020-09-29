1be3d6c6aad6d275cd51556af752cad41d2cc234
Thathsara Hewage <67362351+ThathsaraHewage@users.noreply.github.com>
2020-09-29 15:30:47 +0530

    completed authentications(login,signup)and some changes

--- app/src/main/java/com/example/giftme/AddRider.java
+++ app/src/main/java/com/example/giftme/AddRider.java
@@ -1,5 +1,12 @@
+/*N.I.T.Hewage_IT19220116
+ * MADD mini project 2020_Y2S2_GIFT_ODERIN_GAPP
+ * Delivery managment
+ * */
 package com.example.giftme;
 
+/*
+ * Class for delivery riders' tasks
+ * */
 public class AddRider {
     String name;
     String address;
@@ -9,69 +16,70 @@ public class AddRider {
     String nic;
     String lison;
     int age;
+    //constructor
 
-    public AddRider() {
-    }
+    public AddRider() { }
+
+    //get and set rider's name
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    //get and set rider's address
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
 
+    //get and set rider's phone number
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    //get and set rider's email
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //get and set rider's bike number
     public String getBike() {
         return bike;
     }
-
     public void setBike(String bike) {
         this.bike = bike;
     }
 
+    //get and set rider's nic number
     public String getNic() {
         return nic;
     }
-
     public void setNic(String nic) {
         this.nic = nic;
     }
 
+    //get and set rider's lison number
     public String getLison() {
         return lison;
     }
-
     public void setLison(String lison) {
         this.lison = lison;
     }
 
+    //get and set rider's age
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
--- app/src/main/java/com/example/giftme/Delivery.java
+++ app/src/main/java/com/example/giftme/Delivery.java
@@ -1,5 +1,13 @@
+/*N.I.T.Hewage_IT19220116
+ * MADD mini project 2020_Y2S2_GIFT_ODERIN_GAPP
+ * Delivery managment
+ * */
 package com.example.giftme;
 
+
+/*
+ * Class for delivering tasks
+ * */
 public class Delivery {
     String name;
     String address;
@@ -13,6 +21,7 @@ public class Delivery {
     public Delivery() {
     }
 
+    //get and set name of the  receiver
     public String getName() {
         return name;
     }
@@ -20,51 +29,50 @@ public class Delivery {
         this.name = name;
     }
 
-
+    //get and set address of the receiver
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
 
+    //get and set phone number of the  receiver
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    //get and set item names of the order
     public String getItems() {
         return items;
     }
-
     public void setItems(String items) {
         this.items = items;
     }
 
+    //get and set total price of the order
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
 
+    //get and set assigned delivery bou's id for an oder
     public String getBoyid() {
         return boyid;
     }
-
     public void setBoyid(String boyid) {
         this.boyid = boyid;
     }
 
+    //get and set the date of the delivery
     public String getDate() {
         return date;
     }
-
     public void setDate(String date) {
         this.date = date;
     }
